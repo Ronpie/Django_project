7f081d408cb9dc4d31030dd178f246f87f2902f8
Ronpie <chatzalo123456@gmail.com>
2020-03-15 11:15:08 +0700

    adding half-part-8

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+# After creating model 
+# Remember to makemigrations, migrate
+# Register to admin.py
+class Profile(models.Model):
+	# Delete user will delete the profile
+	# but delete profile not delete user
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	image = models.ImageField(default='default.jpg',upload_to='profile_pics')
 
-# Create your models here.
+	def __str__(self):
+		return f'{self.user.username} Profile'
+
